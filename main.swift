cb6889bec266d583c41aea15a7a6fa02c559f739
Arshia Verma <mishu45r@gmail.com>
2022-01-10 17:58:25 -0500

    main file

---
+++ main.swift
@@ -0,0 +1,20 @@
+/*
+let s = Stats(values: [5.5, 3.2, 3.6, 4.8, 13, 9.1, 19.5, 3.7])
+print("sum: \(s.sum())")
+print("mean: \(s.mean())")
+print("median: \(s.median())")
+print("standard deviation: \(s.stdev())")
+print("primes: \(s.primes())")
+
+
+
+print("Type something", terminator:" -> ")
+var consoleText = readLine()!
+var new = splitStringIntoParts(consoleText)
+var sv = StatsVC()
+print(new)
+print("end")
+*/
+
+var vc = StatsVC()
+vc.runStats()
